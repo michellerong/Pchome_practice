cb1cecfed9c0c8c0618e92050f49693e372af49a
Rong <93319350+michellerong@users.noreply.github.com>
2024-03-03 23:05:53 +0800

    feat: react-player

--- test01/src/pages/Home.js
+++ test01/src/pages/Home.js
@@ -1,4 +1,4 @@
-
+import ReactPlayer from 'react-player';
 
 function Home() {
     return (
@@ -6,7 +6,12 @@ function Home() {
             {/* 設定頁面title */}
            
                 <h2>首頁</h2>
-           
+                <ReactPlayer
+        url="https://youtube.com/shorts/o47ZmlA-CBE?si=y7zf6kCLS5si0m92"  // 或者本地视频文件路径
+        controls={true}
+        width="150px"
+        height="400px"
+      />
             {/* component */}
             
         </div>
